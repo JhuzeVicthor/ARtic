12d03d964db18697e6fb477708805574d628a5ad
Jhuze Victhor <jhuzevicthor63@gmail.com>
2025-07-25 11:13:15 -0300

    carro
    carro controller terminado

--- backend/src/main/java/com/ARtic/ARtic/carrental/controller/CarroController.java
+++ backend/src/main/java/com/ARtic/ARtic/carrental/controller/CarroController.java
@@ -1,10 +1,61 @@
 package com.ARtic.ARtic.carrental.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.ARtic.ARtic.carrental.dto.CarroRequestDTO;
+import com.ARtic.ARtic.carrental.dto.CarroResponseDTO;
+import com.ARtic.ARtic.carrental.services.CarroService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/carros")
 public class CarroController {
 
+    private final CarroService carroService;
+
+    public CarroController(CarroService carroService) {
+        this.carroService = carroService;
+    }
+
+    @PostMapping
+    public ResponseEntity<CarroResponseDTO> createCarro(@Valid @RequestBody CarroRequestDTO carroRequestDTO) {
+        CarroResponseDTO newCarro = carroService.createCarro(carroRequestDTO);
+        return new ResponseEntity<>(newCarro, HttpStatus.CREATED);
+    }
+
+
+    @GetMapping
+    public ResponseEntity<List<CarroResponseDTO>> getAllCarros() {
+        List<CarroResponseDTO> carros = carroService.getAllCarros();
+        return new ResponseEntity<>(carros, HttpStatus.OK);
+    }
+
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CarroResponseDTO> getCarroById(@PathVariable Long id) {
+        return carroService.getCarroById(id)
+                .map(carro -> new ResponseEntity<>(carro, HttpStatus.OK))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+
+    @PutMapping("/{id}")
+    public ResponseEntity<CarroResponseDTO> updateCarro(@PathVariable Long id, @Valid @RequestBody CarroRequestDTO carroRequestDTO) {
+        return carroService.updateCarro(id, carroRequestDTO)
+                .map(updatedCarro -> new ResponseEntity<>(updatedCarro, HttpStatus.OK))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteCarro(@PathVariable Long id) {
+        if (carroService.deleteCarro(id)) {
+            return new ResponseEntity<>(HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
 }
